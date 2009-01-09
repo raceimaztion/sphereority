ec90d3d31894b93e7b52d7fc8d47a2e6c2c533a1
raceimaztion <dvanhumb@sfu.ca>
2009-01-08 22:54:02 -0800

    DO NOT MERGE THIS! I'm completely rewriting the entire network message system, so it's currently very broken. I've successfully "transmitted" a LoginRequestMessage!

--- common/messages/LoginRequestMessage.java
+++ common/messages/LoginRequestMessage.java
@@ -1,11 +1,17 @@
 package common.messages;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidParameterException;
 
 public class LoginRequestMessage extends Message
 {
     private String userName, password;
     
+    /**
+     * Encode a new login request message
+     * @param userName The name the new player would like to use
+     * @param password The optional password to use on the server
+     */
     public LoginRequestMessage(String userName, String password)
     {
         super(TYPE_LOGIN_REQUEST);
@@ -15,6 +21,29 @@ public class LoginRequestMessage extends Message
             this.password = "";
         else
             this.password = password;
+    }
+    
+    
+    public LoginRequestMessage(byte[] message) throws Exception
+    {
+        super(TYPE_LOGIN_REQUEST);
+        
+        if (message[1] != (byte)messageType)
+            throw new InvalidParameterException(String.format("The byte array passed to the LoginRequestMessage class is not a login request message. Message number is 0x%02x.", message[1]));
+        
+        // Ignore the first two bytes, they're the magic number and the message type
+        try
+        {
+            int first, second;
+            for (first=2; first < message.length && message[first] != 0; first++);
+            for (second=first+1; second < message.length && message[second] != 0; second++);
+            userName = new String(message, 2, first-2, STRING_CHARSET);
+            password = new String(message, first+1, second-first-1, STRING_CHARSET);
+        }
+        catch (UnsupportedEncodingException er)
+        {
+            System.err.printf("Fatal error: Character set 'UTF-8' is not supported!");
+        }
     }
     
     public String getUserName()
--- daryl/MessageTest.java
+++ daryl/MessageTest.java
@@ -13,5 +13,16 @@ public class MessageTest
         System.out.printf("Username: '%s', Password: '%s'\n", msg.getUserName(), msg.getPassword());
         for (int i=0; i < b_msg.length; i++)
             System.out.printf("0x%02x  ", b_msg[i]);
+        
+        try
+        {
+            LoginRequestMessage msg2 = new LoginRequestMessage(b_msg);
+            System.out.printf("Transmitted message:\nUsername: '%s', Password: '%s'.\n",
+                    msg2.getUserName(), msg2.getPassword());
+        }
+        catch (Exception er)
+        {
+            er.printStackTrace();
+        }
     }
 }
